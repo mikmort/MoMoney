80e7f2250e8380c5af6b625637948dc16e7fd8b3
Mike Morton <mikmort@hotmail.com>
2025-08-03 18:43:07 +0200

    Fix TypeScript module recognition issue for Transactions component

---
+++ src/components/Transactions/Transactions.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Transactions: React.FC = () => {
+  return (
+    <div>
+      <h1>Transactions</h1>
+      <p>Transactions component is working!</p>
+    </div>
+  );
+};
+
+export default Transactions;
---
+++ src/components/Transactions/TransactionsMinimal.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Transactions: React.FC = () => {
+  return <div>Transactions Component</div>;
+};
+
+export default Transactions;
